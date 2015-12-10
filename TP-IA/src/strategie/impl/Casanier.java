c9257c11e008ad7ba33ae94356988a9e2d0e2624
JbRezidu <duriez.jeanbaptiste@gmail.com>
2015-12-10 11:17:21 +0100

    [JDZ] ADD Casanier Startégie

---
+++ TP-IA/src/strategie/impl/Casanier.java
@@ -0,0 +1,15 @@
+package strategie.impl;
+
+import strategie.StrategieI;
+
+/**
+ * @author DURIEZ Jean-Baptiste et QUINCY Jordane
+ */
+public class Casanier implements StrategieI {
+
+	@Override
+	public boolean allerAuBar() {
+		return false;
+	}
+
+}
