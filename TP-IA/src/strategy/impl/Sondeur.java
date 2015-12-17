e8c8b6f52c9d455f95ed638ab64a48477d3401fd
JbRezidu <duriez.jeanbaptiste@gmail.com>
2015-12-17 11:42:11 +0100

    Add sondeur Fou

--- TP-IA/src/strategy/impl/Sondeur.java
+++ TP-IA/src/strategy/impl/Sondeur.java
@@ -9,44 +9,49 @@ import strategy.StrategieI;
 /**
  * @author DURIEZ Jean-Baptiste et QUINCY Jordane
  */
-public class Sondeur implements StrategieI {
+public abstract class Sondeur implements StrategieI {
 
+  // Must be implemented in sub classes.
 	@Override
-	public boolean goToTheBar(final Person me,
-			final List<Map<Person, Boolean>> historiqueDesTours) {
-		// je reste chez me, ensuite je vais deux fois au bar. Si la majorité
-		// reste chez eux, je reste également chez me (sinon je vais au bar).
+	public abstract boolean goToTheBar(Person me,
+			final List<Map<Person, Boolean>> turnHistoric);
 
-		final int nbTurn = historiqueDesTours.size();
-		if (nbTurn < 1) {
-			// Stay at home at first round
-			return false;
-		} else if (nbTurn <= 3) {
-			// Go to the bar at first round
-			return true;
-		} else {
 
-			int nbTotalPersonAtTheBar = 0;
-			int nbTotalPerson = 0;
-			final Map<Person, Boolean> turn = historiqueDesTours
-					.get(historiqueDesTours.size() - 1);
-			for (final Person i : turn.keySet()) {
-				final boolean estPartiAuBar = turn.get(i);
-				if (estPartiAuBar) {
-					nbTotalPersonAtTheBar++;
-				}
-				nbTotalPerson++;
-			}
+    protected boolean haveToGotToTheBar(final Person me,
+        final List<Map<Person, Boolean>> turnHistoric) {
+      // je reste chez me, ensuite je vais deux fois au bar. Si la majorité
+      // reste chez eux, je reste également chez me (sinon je vais au bar).
 
-			final double ratioNbPersonAtTheBarOnNbPersonAtThisTurn = (double) nbTotalPerson
-					/ nbTotalPersonAtTheBar;
-			System.out.print("( nbTotalPerson /  nbTotalPersonAtTheBar) = ("
-					+ nbTotalPerson + "/" + nbTotalPersonAtTheBar + ") = ");
-			System.out.format("%.3f : ",
-					ratioNbPersonAtTheBarOnNbPersonAtThisTurn);
-			System.out
-					.println(!(ratioNbPersonAtTheBarOnNbPersonAtThisTurn >= 0.5d));
-			return !(ratioNbPersonAtTheBarOnNbPersonAtThisTurn >= 0.5d);
-		}
-	}
+      final int nbTurn = turnHistoric.size();
+      if (nbTurn < 1) {
+        // Stay at home at first round
+        return false;
+      } else if (nbTurn <= 3) {
+        // Go to the bar at first round
+        return true;
+      } else {
+
+        int nbTotalPersonAtTheBar = 0;
+        int nbTotalPerson = 0;
+        final Map<Person, Boolean> turn = turnHistoric
+            .get(turnHistoric.size() - 1);
+        for (final Person i : turn.keySet()) {
+          final boolean estPartiAuBar = turn.get(i);
+          if (estPartiAuBar) {
+            nbTotalPersonAtTheBar++;
+          }
+          nbTotalPerson++;
+        }
+
+        final double ratioNbPersonAtTheBarOnNbPersonAtThisTurn = (double) nbTotalPerson
+            / nbTotalPersonAtTheBar;
+        System.out.print("( nbTotalPerson /  nbTotalPersonAtTheBar) = ("
+            + nbTotalPerson + "/" + nbTotalPersonAtTheBar + ") = ");
+        System.out.format("%.3f : ",
+            ratioNbPersonAtTheBarOnNbPersonAtThisTurn);
+        System.out
+            .println(!(ratioNbPersonAtTheBarOnNbPersonAtThisTurn >= 0.5d));
+        return (ratioNbPersonAtTheBarOnNbPersonAtThisTurn >= 0.5d);
+      }
+    }
 }
---
+++ TP-IA/src/strategy/impl/SondeurFou.java
@@ -0,0 +1,20 @@
+package strategy.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import person.Person;
+import strategy.StrategieI;
+
+/**
+ * @author DURIEZ Jean-Baptiste et QUINCY Jordane
+ */
+public class SondeurFou extends Sondeur {
+	// je reste chez me, ensuite je vais deux fois au bar. Si la majorité
+	// reste chez eux, je vais au bar (sinon je reste chez moi)
+	@Override
+	public boolean goToTheBar(final Person me,
+			final List<Map<Person, Boolean>> turnHistoric) {
+		return this.haveToGoToTheBar(turnHistoric);
+	}
+}
